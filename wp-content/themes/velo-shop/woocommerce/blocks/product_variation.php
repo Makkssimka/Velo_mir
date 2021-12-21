f000ea4f05865538bbe9b7ca1777e93e866160fb
Makkssimka <swoisait@gmail.com>
2021-12-21 11:38:13 +0300

    Добавил правильное отображение вариаций

--- wp-content/themes/velo-shop/woocommerce/blocks/product_variation.php
+++ wp-content/themes/velo-shop/woocommerce/blocks/product_variation.php
@@ -1,37 +1,71 @@
 <?php
 
 $tags = wp_get_object_terms( $product->get_id(), 'product_tag');
-$tag = array_shift($tags); ?>
+$tag = array_shift($tags);
+
+$product_diametr = $product->get_attribute('diametr-kolesaoboda');
+$product_cvet = $product->get_attribute('cvet');
+
+?>
 
 <?php if ($tag) : ?>
 
 <?php
 
-$products_tag = wc_get_products(array(
+$products_by_tag = wc_get_products(array(
     'product_tag' => $tag->slug,
+    'stock_status' => 'instock',
 ));
 
-$product_colors = array();
-$colors = array();
+$product_variations = [];
 
-foreach ($products_tag as $item) {
-    $color = $item->get_attribute('cvet');
-    if (!in_array($color, $colors)) {
-        $colors[] = $color;
-        $product_colors[] = $item;
+foreach ($products_by_tag as $prod) {
+    $d = $prod->get_attribute('diametr-kolesaoboda');
+    $c = $prod->get_attribute('cvet');
+    if (!key_exists($d, $product_variations)) {
+        $product_variations[$d] = [
+            'id' => $prod->get_id(),
+            'cvets' => [
+                [
+                    'id' => $prod->get_id(),
+                    'cvet' => $c
+                ]
+            ]
+        ];
+    } else {
+        $product_variations[$d]['cvets'][] = [
+            'id' => $prod->get_id(),
+            'cvet' => $c
+        ];
     }
 }
+
+ksort($product_variations);
+
 ?>
 
-<div class="product-color">
-    <p>цвет:</p>
+<div class="product-size">
+    <p>Размер колес:</p>
     <ul>
-        <?php foreach ($product_colors as $item) : ?>
-            <li class="<?= $item->get_attribute('cvet') == $product->get_attribute('cvet')?'inactive-element':'' ?>">
-                <?= get_color_link($item->get_id()) ?>
+        <?php foreach($product_variations as $key => $item) : ?>
+            <li class="<?= $key == $product_diametr ? 'active' : '' ?>">
+                <a href="<?= get_permalink($item['id']) ?>">
+                    <?= $key ?>
+                </a>
             </li>
         <?php endforeach; ?>
     </ul>
 </div>
 
-<?php endif; ?>
+<div class="product-color">
+    <p>цвет:</p>
+    <ul>
+        <?php foreach ($product_variations[$product_diametr]['cvets'] as $item) : ?>
+            <li class="<?= $item['cvet'] == $product_cvet ? 'inactive-element' : '' ?>">
+                <?= get_color_link($item['id']) ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</div>
+
+<?php endif ?>
