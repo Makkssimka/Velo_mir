8cc4de52cd7e5eec7a10f309ccae7935f8d23e5a
Makkssimka <swoisait@gmail.com>
2022-12-09 18:36:18 +0300

    clean memory

--- wp-content/plugins/importer/admin/js/importer-admin.js
+++ wp-content/plugins/importer/admin/js/importer-admin.js
@@ -44,7 +44,7 @@ $(document).ready(function () {
 	const progressMsg = $('.progress-message');
 	const step = 10;
 	let globalStep = 0
-	const globalCounterStep = 500;
+	const globalCounterStep = 100;
 	let globalCountProduct = 0;
 	let importTimer = 0;
 	let importCategoryTimer = 0;
