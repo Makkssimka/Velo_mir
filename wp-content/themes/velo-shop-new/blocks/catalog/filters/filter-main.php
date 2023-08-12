fb87ca4643bfe522458e5f83607575cf55a2f50e
Makkssimka <swoisait@gmail.com>
2023-08-12 16:32:06 +0300

    test

--- wp-content/themes/velo-shop-new/blocks/catalog/filters/filter-main.php
+++ wp-content/themes/velo-shop-new/blocks/catalog/filters/filter-main.php
@@ -26,7 +26,7 @@ foreach ($products_filter as $product) {
     $attributes = $product->get_attributes();
 
     foreach ($attributes as $slug => $attribute) {
-        if ($slug === 'pa_cvet_dop' || empty($attribute['options'])) continue;
+        if ($slug === 'pa_cvet_dop' || $slug === 'pa_' || empty($attribute['options'])) continue;
 
         if (in_array($slug, $products_attribute_list)) {
             $products_attribute_list[$slug]['terms'][] = $attribute;
