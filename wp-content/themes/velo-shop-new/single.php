c344ae53a528a8186180a8fede05e03526273332
Makkssimka <swoisait@gmail.com>
2023-09-18 10:17:53 +0300

    Отображение записи

--- wp-content/themes/velo-shop-new/single.php
+++ wp-content/themes/velo-shop-new/single.php
@@ -1,28 +1,17 @@
 <?php
 global $post;
-$name_expert = get_option('name_expert');
 ?>
 
 <?php get_header(); ?>
 
-<div class="content-main article">
-    <div class="container">
-        <h1><?= $post->post_title; ?></h1>
-        <div class="article-subheader"><?= $post->post_excerpt; ?></div>
-        <div class="article-content">
-            <div class="article-text">
-                <?= $post->post_content; ?>
-            </div>
-            <div class="article-navbar">
-                <?php expert_widget() ?>
-                <div class="article-anchor-nav">
-                    <p>Содержание:</p>
-                    <ul class="anchor-list">
+<?php get_template_part('blocks/breadcrumbs') ?>
 
-                    </ul>
-                </div>
-            </div>
-        </div>
+<div class="container content">
+    <h1 class="h3"><?= $post->post_title; ?></h1>
+    <div class="h6"><?= $post->post_excerpt; ?></div>
+
+    <div>
+        <?= $post->post_content; ?>
     </div>
 </div>
 
